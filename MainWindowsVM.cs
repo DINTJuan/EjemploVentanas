374f2bf3c3b2b26df4e9f20671048e0ef1c84082
JuanMateoSanchez <90640548+JuanMateoSanchez@users.noreply.github.com>
2023-01-10 15:04:11 +0100

    Agregar archivos de proyecto.

---
+++ MainWindowsVM.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploVentanas
+{
+    class MainWindowsVM : ObservableObject
+    {
+        public RelayCommand AbrirHijaCommand { get; }
+
+        private ServicioNavegacion navegacion;
+
+        public MainWindowsVM()
+        {
+            AbrirHijaCommand = new RelayCommand(AbrirHija);
+            navegacion = new ServicioNavegacion();
+        }
+
+        public void AbrirHija()
+        {
+            navegacion.AbrirVentanaHija();
+        }
+    }
+}
---
+++ ServicioNavegacion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploVentanas
+{
+    class ServicioNavegacion
+    {
+        public bool? AbrirVentanaHija()
+        {
+            VentanaHija nueva = new VentanaHija();
+            return nueva.ShowDialog();
+        }
+    }
+}
